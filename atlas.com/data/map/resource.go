624df001e98e7ebddc7b3ce81abead95b8863acd
Chronicle20 <a.chronicle.20@gmail.com>
2025-03-10 11:04:17 -0400

    Add endpoint to retrieve foothold below point in map.

--- atlas.com/data/map/resource.go
+++ atlas.com/data/map/resource.go
@@ -32,6 +32,7 @@ func InitResource(si jsonapi.ServerInformation) server.RouteInitializer {
 		r.HandleFunc("/{mapId}/npcs/{npcId}", registerGet("get_map_npc", handleGetMapNPCRequest)).Methods(http.MethodGet)
 		r.HandleFunc("/{mapId}/monsters", registerGet("get_map_monsters", handleGetMapMonstersRequest)).Methods(http.MethodGet)
 		r.HandleFunc("/{mapId}/drops/position", rest.RegisterInputHandler[DropPositionRestModel](l)(si)("get_map_drop_position", handleGetMapDropPositionRequest)).Methods(http.MethodPost)
+		r.HandleFunc("/{mapId}/footholds/below", rest.RegisterInputHandler[PositionRestModel](l)(si)("get_map_foothold_below", handleGetMapFootholdBelowRequest)).Methods(http.MethodPost)
 	}
 }
 
@@ -278,6 +279,81 @@ func (r DropPositionRestModel) GetID() string {
 }
 
 func (r *DropPositionRestModel) SetID(strId string) error {
+	id, err := strconv.Atoi(strId)
+	if err != nil {
+		return err
+	}
+	r.Id = uint32(id)
+	return nil
+}
+
+func handleGetMapFootholdBelowRequest(d *rest.HandlerDependency, c *rest.HandlerContext, i PositionRestModel) http.HandlerFunc {
+	return rest.ParseMapId(d.Logger(), func(mapId uint32) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			m, err := GetById(d.Context())(mapId)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			p := point.NewModel(i.X, i.Y)
+			fh := m.footholdTree.findBelow(p)
+			if fh == nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			rm := FootholdRestModel{
+				Id: fh.id,
+				X1: fh.first.X(),
+				Y1: fh.first.Y(),
+				X2: fh.second.X(),
+				Y2: fh.second.Y(),
+			}
+			server.Marshal[FootholdRestModel](d.Logger())(w)(c.ServerInformation())(rm)
+		}
+	})
+}
+
+type PositionRestModel struct {
+	Id uint32 `json:"-"`
+	X  int16  `json:"x"`
+	Y  int16  `json:"y"`
+}
+
+func (r PositionRestModel) GetName() string {
+	return "positions"
+}
+
+func (r PositionRestModel) GetID() string {
+	return strconv.Itoa(int(r.Id))
+}
+
+func (r *PositionRestModel) SetID(strId string) error {
+	id, err := strconv.Atoi(strId)
+	if err != nil {
+		return err
+	}
+	r.Id = uint32(id)
+	return nil
+}
+
+type FootholdRestModel struct {
+	Id uint32 `json:"-"`
+	X1 int16  `json:"x1"`
+	Y1 int16  `json:"y1"`
+	X2 int16  `json:"x2"`
+	Y2 int16  `json:"y2"`
+}
+
+func (r FootholdRestModel) GetName() string {
+	return "footholds"
+}
+
+func (r FootholdRestModel) GetID() string {
+	return strconv.Itoa(int(r.Id))
+}
+
+func (r *FootholdRestModel) SetID(strId string) error {
 	id, err := strconv.Atoi(strId)
 	if err != nil {
 		return err
